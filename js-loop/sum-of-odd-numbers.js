c92fcd5afbdb6f2e87396ea7f4bf3269603bafad
galive007 <Alamingalive007@gmail.com>
2024-03-18 00:45:41 +0600

    task looping

--- js-loop/sum-of-odd-numbers.js
+++ js-loop/sum-of-odd-numbers.js
@@ -1,9 +1,24 @@
-let num = 61;
+let num = 81;
 let sum=0;
-while (num<= 100) {
+while (num<= 131) {
     if (num % 2 === 1 ) {
         console.log('Odd Numbers: ',num);
+        sum=sum+num;   
     }
     num++;
-    
 }
+console.log(sum);
+
+
+console.log('**************');
+
+let num2 = 206;
+let sum2=0;
+while (num2<= 311) {
+    if (num2 % 2 === 0 ) {
+        console.log('Odd Numbers: ',num2);
+        sum2=sum2+num2;   
+    }
+    num2++;
+}
+console.log(sum2);
